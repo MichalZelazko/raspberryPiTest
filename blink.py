b55bb9c2a5e6579341c1e29fa18e37587f2f4cd9
MichalZelazko <242149@edu.p.lodz.pl>
2022-06-19 12:35:39 +0200

    blinkonbutton

--- blink.py
+++ blink.py
@@ -3,9 +3,11 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
-for i in range(10):
-	GPIO.output(23, True)
-	time.sleep(1)
-	GPIO.output(23, False)
-	time.sleep(1)
-GPIO.cleanup()
+try:
+	while(True):
+		GPIO.output(23, False)
+		while(GPIO.input(4) == GPIO.LOW):
+			GPIO.output(23, True)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+		
